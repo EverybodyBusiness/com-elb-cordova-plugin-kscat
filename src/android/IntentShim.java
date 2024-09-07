f3f9826f5f1b54c518b0d99cd0ed4bb66bb73eeb
byoungchullee <bckorea@outlook.com>
2024-09-07 11:44:37 +0900

    1.2.7 again

--- src/android/IntentShim.java
+++ src/android/IntentShim.java
@@ -830,7 +830,7 @@ public class IntentShim extends CordovaPlugin {
 
                 intent.putExtra("transactionCode", new String(trData.transactionCode));
                 intent.putExtra("resultCode", resultCode);
-                intent.putExtra("version", "1.2.6");
+                intent.putExtra("version", "1.2.7");
                 intent.putExtra("package_name", "com.elb.payment2");
                 PluginResult result = new PluginResult(PluginResult.Status.OK, getIntentJson(intent));
                 result.setKeepCallback(true);
